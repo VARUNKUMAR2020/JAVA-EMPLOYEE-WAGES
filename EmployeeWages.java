7f6e6c9a9b8331d6907c76b15b4d4c6190437c9e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-14 20:30:19 +0530

    Add: To calculate Wages using Switch case

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -1,23 +1,32 @@
 package com.employeewages;
 
 public class EmployeeWages {
+    public static final int partTime = 2;
+    public static final int fullTime = 1;
+    public static final int employeeRate = 20;
+    public static int employeehours = 0;
+    public static int employeeWages = 0;
+
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // To check Employee is present or absent using random
-        double fullTime = 1;
-        double randomCheck = Math.floor(Math.random() * 10) % 3;
+        int randomCheck = (int) Math.floor(Math.random() * 10) % 3;
         String check = " ";
         System.out.println(check = (fullTime == randomCheck) ? "Employee is Present" : "Employee is Absent");
 
-        // Calculate Daily Wages
-        int employeeWages = 0;
-        int employeehours = 0;
-        int employeeRate = 20;
+        // TO check Wages of Full & Part Time using switch case
+        switch (randomCheck) {
+            case fullTime:
+                employeehours = 8;
+                break;
+            case partTime:
+                employeehours = 4;
+                break;
+            default:
+                employeehours = 0;
 
-        // TO check Wages of Full & Part Time
-        double partTime = 2;
-        employeehours = (randomCheck == 0) ? 0 : (randomCheck == fullTime) ? 8 : 4;// ternary operator
+        }
         employeeWages = employeehours * employeeRate;
         System.out.println("Employee Wage is " + employeeWages);
 
