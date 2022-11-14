a217e45e373f35c308dc20d98e7816b5b289eee0
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-14 15:16:50 +0530

    Add: To calculate Daily Wages

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -8,6 +8,15 @@ public class EmployeeWages {
         double present = 1;
         double randomCheck = Math.floor(Math.random() * 10) % 2;
         String check = " ";
-        System.out.println(check = (present == randomCheck) ? "Employee is Present" : "Employee is Absent");//Ternary operator
+        System.out.println(check = (present == randomCheck) ? "Employee is Present" : "Employee is Absent");
+
+        // Calculate Daily Wages
+        int employeeWages = 0;
+        int employeehours = 0;
+        int employeeRate = 20;
+        employeehours = (randomCheck == present) ? 8 : 0;// ternary operator
+        employeeWages = employeehours * employeeRate;
+        System.out.println("Employee Wage is " + employeeWages);
+
     }
 }
