96059f15f28f6e113b076a207415c8fa96ae987e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-14 14:53:42 +0530

    Add: To check Present or Absent

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -3,5 +3,11 @@ package com.employeewages;
 public class EmployeeWages {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
+
+        // To check Employee is present or absent using random
+        double present = 1;
+        double randomCheck = Math.floor(Math.random() * 10) % 2;
+        String check = " ";
+        System.out.println(check = (present == randomCheck) ? "Employee is Present" : "Employee is Absent");//Ternary operator
     }
 }
