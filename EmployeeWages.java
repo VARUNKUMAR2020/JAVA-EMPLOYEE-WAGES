e67c52505ecb962bc36a76dec1f10ec37e7780bf
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-14 14:28:52 +0530

    Employee Wage Computation program

---
+++ EmployeeWages.java
@@ -0,0 +1,7 @@
+package com.employeewages;
+
+public class EmployeeWages {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation Program");
+    }
+}
