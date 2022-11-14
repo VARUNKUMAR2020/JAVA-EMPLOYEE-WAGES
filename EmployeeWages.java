635e1ec23f7054a9d50ee74e5e8be45a91f39cff
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-14 16:38:28 +0530

    Add: To calculate Wages for Partime and Fulltime

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -5,16 +5,19 @@ public class EmployeeWages {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // To check Employee is present or absent using random
-        double present = 1;
-        double randomCheck = Math.floor(Math.random() * 10) % 2;
+        double fullTime = 1;
+        double randomCheck = Math.floor(Math.random() * 10) % 3;
         String check = " ";
-        System.out.println(check = (present == randomCheck) ? "Employee is Present" : "Employee is Absent");
+        System.out.println(check = (fullTime == randomCheck) ? "Employee is Present" : "Employee is Absent");
 
         // Calculate Daily Wages
         int employeeWages = 0;
         int employeehours = 0;
         int employeeRate = 20;
-        employeehours = (randomCheck == present) ? 8 : 0;// ternary operator
+
+        // TO check Wages of Full & Part Time
+        double partTime = 2;
+        employeehours = (randomCheck == 0) ? 0 : (randomCheck == fullTime) ? 8 : 4;// ternary operator
         employeeWages = employeehours * employeeRate;
         System.out.println("Employee Wage is " + employeeWages);
 
