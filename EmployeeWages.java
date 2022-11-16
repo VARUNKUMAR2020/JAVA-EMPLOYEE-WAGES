f36c32f708013ad77db5bb3602d7ef106798a7ad
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-16 09:52:59 +0530

    Add: To calculate Total Wages of 20 DAys

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -6,15 +6,18 @@ public class EmployeeWages {
     public static final int employeeRate = 20;
     public static int employeehours = 0;
     public static int employeeWages = 0;
+    public static int numberOfWorkingDays = 20;
+    public static int totalEmployeeWages = 0;
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         // To check Employee is present or absent using random
         int randomCheck = (int) Math.floor(Math.random() * 10) % 3;
-        String check = " ";
-        System.out.println(check = (fullTime == randomCheck) ? "Employee is Present" : "Employee is Absent");
+        System.out.println((fullTime == randomCheck || partTime == randomCheck) ? "Employee is Present" : "Employee is Absent");
 
+        //Working days is 20
+        for(int day=0;day<=numberOfWorkingDays;day++){
         // TO check Wages of Full & Part Time using switch case
         switch (randomCheck) {
             case fullTime:
@@ -28,7 +31,10 @@ public class EmployeeWages {
 
         }
         employeeWages = employeehours * employeeRate;
+        totalEmployeeWages += employeeRate;
         System.out.println("Employee Wage is " + employeeWages);
+    }
+    System.out.println("Total Employee Wage is " +totalEmployeeWages );
 
     }
 }
