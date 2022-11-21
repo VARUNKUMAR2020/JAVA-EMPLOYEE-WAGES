997d05f8eed2ec148301f09d28058f62449d3b35
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-21 22:59:37 +0530

    Add: Employee Wages for Multiple Companies

--- EmployeeWages.java
+++ EmployeeWages.java
@@ -9,43 +9,46 @@ public class EmployeeWages {
     public static int numberOfWorkingDays = 20;
     public static int totalEmployeeWages = 0;
 
-    public static void employeeWagesCalculation(){
-      // To check Employee is present or absent using random
-      int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
-      String check;
-      System.out.println(check = (fullTime == randomCheck || partTime == randomCheck) ? "Employee is Present"
-              : "Employee is Absent");
+    public static void employeeWagesCalculation(String companyName, int employeeRate, int numberOfWorkingDays, int employeehours) {
+        System.out.println(companyName + " Employee Wage Computation :");
+        // To check Employee is present or absent using random
+        int randomCheck = (int) Math.floor(Math.random() * 10) % 2;
+        String check;
+        System.out.println(check = (fullTime == randomCheck || partTime == randomCheck) ? "Employee is Present"
+                : "Employee is Absent");
 
-      // Working hours is 100
-      int hoursInMonth = 100, workingDays = 0;
-      while (employeehours <= hoursInMonth && workingDays < numberOfWorkingDays) {
-          workingDays++;
-          int optioncheck = (int) Math.floor(Math.random() * 10) % 3;
-          // TO check Wages of Full & Part Time using switch case
-          switch (optioncheck) {
-              case fullTime:
-                  employeehours = 8;
-                  break;
-              case partTime:
-                  employeehours = 4;
-                  break;
-              default:
-                  employeehours = 0;
+        // Working hours is 100
+        int hoursInMonth = 100, workingDays = 0;
+        while (employeehours <= hoursInMonth && workingDays < numberOfWorkingDays) {
+            workingDays++;
+            int optioncheck = (int) Math.floor(Math.random() * 10) % 3;
+            // TO check Wages of Full & Part Time using switch case
+            switch (optioncheck) {
+                case fullTime:
+                    employeehours = 8;
+                    break;
+                case partTime:
+                    employeehours = 4;
+                    break;
+                default:
+                    employeehours = 0;
 
-          }
-          totalEmployeeWages += employeeRate;
-          System.out.println("Day:" +workingDays + "  Hours: " + employeeRate);
-      }
+            }
+            totalEmployeeWages += employeeRate;
+            System.out.println("Day:" + workingDays + "  Hours: " + employeeRate);
+        }
 
-      int totalEmpWages = totalEmployeeWages * employeeRate;
-      System.out.println("Total employee wages is " + totalEmpWages);
+        int totalEmpWages = totalEmployeeWages * employeeRate;
+        System.out.println("Total employee wages is " + totalEmpWages);
+        System.out.println("------------------------------------------");
     }
 
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
-        employeeWagesCalculation();
+        employeeWagesCalculation("Company A",20,20,100);
+        employeeWagesCalculation("Company B",52,20,80);
+        employeeWagesCalculation("Company C",22,26,60);
 
-      
     }
 
 }
